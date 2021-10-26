3b5b6183a277b8fa09341b7daa49abb4bb930f2c
sodeypops <33789541+sodeypops@users.noreply.github.com>
2021-10-26 09:37:11 +0100

    added additional test case for key37

--- java_scala/src/test/scala/com/intenthq/challenge/SEnigmaSpec.scala
+++ java_scala/src/test/scala/com/intenthq/challenge/SEnigmaSpec.scala
@@ -6,6 +6,7 @@ class SEnigmaSpec extends Specification {
 
   section("scala")
   section("enigma")
+
   "SEnigmaSpec" should {
     val map = Map(23 -> 'N', 234 -> ' ', 89 -> 'H', 78 -> 'Q', 37 -> 'A')
     val deciphe = SEnigma.deciphe(map) _
@@ -13,6 +14,7 @@ class SEnigmaSpec extends Specification {
     "(2,3) is 'N'" in {
       deciphe(List(2,3)) must_== "N"
     }
+
     "(2,3,8,9) is 'NH'" in {
       deciphe(List(2,3,8,9)) must_== "NH"
     }
@@ -27,6 +29,14 @@ class SEnigmaSpec extends Specification {
 
     "(1,2,3,7,3,2,3,7,2,3,4,8,9,7,8) is '1N73N7 HQ'" in {
       deciphe(List(1,2,3,7,3,2,3,7,2,3,4,8,9,7,8)) must_== "1N73N7 HQ"
+    }
+    
+    "(3,7) is 'A'" in {
+      deciphe(List(3,7)) must_== "A"
+    }
+
+    "(2,3,7,3) is 'N73'" in {
+      deciphe(List(2,3,7,3)) must_== "N73"
     }
   }
   section("enigma")
